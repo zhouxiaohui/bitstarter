50270db29fcc34df71c442c04a26978764b7ff5f
Michael Zhou <michael.zhou@gmail.com>
2013-07-08 21:46:10 +0000

    Returned index.html in web.js

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var buff = '';
+  fs.readFileSync('index.html', function (err, data) {
+    if (err) throw err;
+    buff = data;
+  });
+  respsonse.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
