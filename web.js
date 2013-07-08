46feb0155fa5abaf52a18437695f65e6c754409f
Michael Zhou <michael.zhou@gmail.com>
2013-07-08 21:53:09 +0000

    Fixed buffer toString encoding typo problem

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ app.get('/', function(request, response) {
     if (err) throw err;
     buff = data;
   });
-  respsonse.send(buff.toString('utf-8'));
+  respsonse.send(buff.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
